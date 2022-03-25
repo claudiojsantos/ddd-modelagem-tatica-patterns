3d33971d7bb161975023f8124f846368bf1ec63f
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-03-25 00:00:19 -0300

    fix: error validate ts and jest

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,13 @@ import Order from './entities/order';
 import OrderItem from './entities/order_item';
 
 let customer = new Customer('123', 'Cláudio Santos');
-const address = new Address('Rua José de Alencar', '436', 'Maceio')
+const address = new Address('Rua José de Alencar', '436', '57051-565', 'Maceio');
 
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem('1', 'item 01', 100.00)
-const item2 = new OrderItem('2', 'item 02', 110.00)
-const item3 = new OrderItem('3', 'item 03', 120.00)
+const item1 = new OrderItem('1', 'item 01', 100.0, 'p1', 2);
+const item2 = new OrderItem('2', 'item 02', 110.0, 'p2', 4);
+const item3 = new OrderItem('3', 'item 03', 120.0, 'p3', 2);
 
 const order = new Order('1', '123', [item1, item2, item3]);
-
